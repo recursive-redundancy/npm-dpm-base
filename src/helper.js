d884aebee7e27c2cc31723755951e3ae69c89cbb
recursive-redundancy <danielmorgan.dev@gmail.com>
2018-11-19 13:21:31 -0600

    helper.js: finalized jsDoc comments and helper.js functions

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,5 @@
 /**
- * Helper Module. Contains logic used package-wide.
+ * Helper Module. Contains helper logic used package-wide.
  * @module helper
  */
 
@@ -16,11 +16,13 @@ function isValEmpty(value) {
   return false;
 }
 
-
-/*
-* Replacement for String.prototype.repeat behavior
-* which is not supported in older browsers
-*/
+/**
+ * Replacement for String.prototype.repeat behavior
+ * which is not supported in older browsers
+ * @param {string|number} times - How many times to repeat value
+ * @param {string} value - Value to be repeated
+ * @returns {string}
+ */
 function repeat(times, value) {
   if (isValEmpty(value) || isValEmpty(times) || times == 0) return "";
   if (times == 1) return value;
@@ -48,26 +50,23 @@ function repeat(times, value) {
 function stripValue(value, validator) {
   if (isValEmpty(value) || !validator(value)) return null;
   value = value.toString();
-   return trimLeadingZeroes(value);
+  return trimLeadingZeroes(value);
 }
 
 
 /**
-  * Trims off any leading zeroes in a value
-  * Will leave one leading zero if it is not proceeded by
-  * any other characters - effectively leaving a lone zero.
-  * @param {string|number} value - Value to be trimmed
-  * @returns {string|null} Value trimmed of leading zeros or null is value is empty
-  */
+ * Trims off any leading zeroes in a value
+ * Will leave one leading zero if it is not proceeded by
+ * any other characters - effectively leaving a lone zero.
+ * @param {string|number} value - Value to be trimmed
+ * @returns {string|null} Value trimmed of leading zeros or null is value is empty
+ */
 function trimLeadingZeroes(value) {
   if (isValEmpty(value)) return null;
   return value.replace(/^0*(?=\w)/, '');
 }
 
 
-/*
-* EXPORTS
-*/
 module.exports = {
   repeat,
   stripValue,
